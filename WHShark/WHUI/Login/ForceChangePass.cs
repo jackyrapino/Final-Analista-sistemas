794683e845c389f6faf499a6150854b543787336
jackyrapino <jackymarquez16@gmail.com>
2026-01-10 16:14:51 -0300

    ddelete old front

---
+++ WHShark/WHUI/Login/ForceChangePass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WHUI.Login
+{
+    public partial class ForceChangePass : Form
+    {
+        public ForceChangePass()
+        {
+            InitializeComponent();
+        }
+    }
+}
